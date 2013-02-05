639d35fc2692a2d25317cd5d3ed10df6194270bd
yasu-s <yasuyuki.saito.2012@gmail.com>
2013-02-05 22:00:33 +0900

    Fixed #3

--- src/main/java/org/jenkinsci/plugins/vs_code_metrics/util/CodeMetricsUtil.java
+++ src/main/java/org/jenkinsci/plugins/vs_code_metrics/util/CodeMetricsUtil.java
@@ -3,6 +3,7 @@ package org.jenkinsci.plugins.vs_code_metrics.util;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import javax.xml.bind.JAXB;
 
@@ -21,13 +22,13 @@ public abstract class  CodeMetricsUtil {
      * @return
      * @throws IOException
      */
-    public static CodeMetricsReport createCodeMetricsReport(FilePath path) throws IOException {
+    public static CodeMetricsReport createCodeMetricsReport(FilePath path) {
         InputStream stream = null;
         try {
             stream = path.read();
             return JAXB.unmarshal(stream, CodeMetricsReport.class);
         } catch (IOException e) {
-            throw e;
+            return null;
         } finally {
             try { if (stream != null) stream.close(); } catch (Exception e) {}
         }
@@ -62,13 +63,18 @@ public abstract class  CodeMetricsUtil {
         try {
             FilePath[] reports = getReports(reportFolder);
 
-            CodeMetricsReport total = new CodeMetricsReport();
+            CodeMetricsReport total = null;
 
             for (FilePath report : reports) {
                 CodeMetricsReport bean = createCodeMetricsReport(report);
+                if (bean == null) continue;
 
-                for (Target target : bean.getTargets().getTarget()) {
-                    total.getTargets().getTarget().add(target);
+                if (total != null) {
+                    for (Target target : bean.getTargets().getTarget()) {
+                        total.getTargets().getTarget().add(target);
+                    }
+                } else {
+                    total = bean;
                 }
             }
 
@@ -80,4 +86,63 @@ public abstract class  CodeMetricsUtil {
         }
     }
 
+    /**
+     *
+     * @param  metrics
+     * @return
+     */
+    public static String getMaintainabilityIndex(List<Metric> metrics) {
+        return getMetricValue(Constants.MAINTAINABILITY_INDEX, metrics);
+    }
+
+    /**
+     *
+     * @param  metrics
+     * @return
+     */
+    public static String getCyclomaticComplexity(List<Metric> metrics) {
+        return getMetricValue(Constants.CYCLOMATIC_COMPLEXITY, metrics);
+    }
+
+    /**
+     *
+     * @param  metrics
+     * @return
+     */
+    public static String getClassCoupling(List<Metric> metrics) {
+        return getMetricValue(Constants.CLASS_COUPLING, metrics);
+    }
+
+    /**
+     *
+     * @param metrics
+     * @return
+     */
+    public static String getDepthOfInheritance(List<Metric> metrics) {
+        return getMetricValue(Constants.DEPTH_OF_INHERITANCE, metrics);
+    }
+
+    /**
+     *
+     * @param metrics
+     * @return
+     */
+    public static String getLinesOfCode(List<Metric> metrics) {
+        return getMetricValue(Constants.LINES_OF_CODE, metrics);
+    }
+
+    /**
+     *
+     * @param  name
+     * @param  metrics
+     * @return
+     */
+    public static String getMetricValue(String name, List<Metric> metrics) {
+        for (Metric metric : metrics) {
+            if (metric.getName().equals(name)) {
+                return metric.getValue();
+            }
+        }
+        return "";
+    }
 }
--- src/main/java/org/jenkinsci/plugins/vs_code_metrics/util/Constants.java
+++ src/main/java/org/jenkinsci/plugins/vs_code_metrics/util/Constants.java
@@ -10,7 +10,23 @@ public abstract class Constants {
     /** TOOL URL */
     public static final String ACTION_URL = "vs_code_metrics";
 
-    /** */
+    /** TOOL ICON */
     public static final String ACTION_ICON = "graph.gif";
+
+
+    /** MaintainabilityIndex */
+    public static final String MAINTAINABILITY_INDEX= "MaintainabilityIndex";
+
+    /** CyclomaticComplexity */
+    public static final String CYCLOMATIC_COMPLEXITY = "CyclomaticComplexity";
+
+    /** ClassCoupling */
+    public static final String CLASS_COUPLING = "ClassCoupling";
+
+    /** DepthOfInheritance */
+    public static final String DEPTH_OF_INHERITANCE = "DepthOfInheritance";
+
+    /** LinesOfCode */
+    public static final String LINES_OF_CODE = "LinesOfCode";
 
 }
